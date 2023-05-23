d253bcf20ad7eaf05d33d489ef7c686a7b85132a
Your Name <you@example.com>
2023-05-24 00:09:18 +0200

    -

--- main.py
+++ main.py
@@ -22,6 +22,17 @@ Assumptions:
     requests library is used for fetching html,
     implementing http redirect and other logic on sockets would take too much time
 
+    every occurence of < , followed by optional / for closing tags, followed by any set of characters
+    that does not start with space, followed by >
+    is considered a xml tag and is not considered - in this exact way
+
+    additionally, there is a set of predefined types of tags (like <style> tags) where additionally to
+    ignoring to the tag as described above, the content within the tag is ignored
+    this set of excluded tags is parameterizable.
+
+    it is assumed that for excluded tags, such tag will never have a nested child of the same type,
+    e.g. no <style> inside of <style>
+
 """
 import requests
 
@@ -29,7 +40,12 @@ import requests
 class Tag:
 
     def __init__(self, tagType):
-        self.tagType = tagType
+        if tagType:
+            self.tagType = tagType
+        else:
+            self.tagType = ""
+
+        self.consideredTagType = ""
 
         self.openingTagOpened = False
         self.openingTagClosed = False
@@ -39,20 +55,36 @@ class Tag:
 
         self.position = 0
 
+        self.appendTagType = True
+
+        # self.wrong = False  # Not sure myself about this
+
     def handle_letter(self, letter):
         if letter == self.tagType[self.position]:
             self.position += 1
             if self.position == len(self.tagType):
                 self.close_tag()
+        else:
+            # if other than next expected letter, reset position
+            self.position = 0
+
+    def handle_letter_unknown_tag(self, letter):
+        if letter == " ":
+            # dont append to tagType anymore (class=,id= etc.)
+            self.appendTagType = False
+        if not self.appendTagType:
+            return
+        if not self.openingTagClosed:
+            self.tagType += letter
+        else:
+            self.consideredTagType += letter
 
     def close_tag(self):
         if not self.openingTagClosed:
             self.openingTagClosed = True
         else:
             self.closingTagClosed = True
-
-
-
+        self.appendTagType = True
 
 class Extractor:
 
@@ -62,11 +94,8 @@ class Extractor:
 
         self.html = None
         self.url = url
-
         self.load_html()
-
-        self.body_tag = Tag("body")
-
+        self.body_tag = Tag("body ")
         self.current_child_tag = None
 
     def load_html(self):
@@ -76,12 +105,85 @@ class Extractor:
 
     def run(self):
 
-        bodyStartsAt = None
-        for index, char in enumerate(self.html):
-            if not self.body_tag.openingTagClosed:
+        isNotBodyTag = False
+
+        tagOpen = False
+
+        for index, letter in enumerate(self.html):
+            if not tagOpen:
+                if letter == "<":
+                    tagOpen = True
+            else:
+                if letter == ">":
+                    tagOpen = False
+                    isNotBodyTag = False
+                    if self.body_tag.openingTagClosed:
+                        # we found the index from which to look for readable words
+                        index += 1
+                        break
+                    continue
+                if not isNotBodyTag:
+                    if self.body_tag.openingTagClosed:
+                        # allow all letters until closing >
+                        # for example <body class="SomeClass">
+                        continue
+                    self.body_tag.handle_letter(letter)
+                    if self.body_tag.position == 0:
+                        isNotBodyTag = True
+
+
+        # We are within the body. We can start stripping other tags now.
+        someTag = Tag(None)
+        tagOpen = False
+        tagOpenIndex = None
+        ignoreTagContent = False
+
+        for index in range(index, len(self.html)):
+            if index == 73727:
                 print(1)
-            elif self.body_tag.closingTagClosed:
-                print(1)
+            letter = self.html[index]
+
+            if ignoreTagContent:
+                # only run rest of the loop if maybe current excluded tag is closed
+                # we assume one excluded type is not nested inside another excluded tag, where both of same type
+                if not tagOpen:
+                    if letter != "<":
+                        continue
+                    else:
+                        print(1)
+                else:
+                    print(1)
+
+            if not tagOpen:
+                if letter == "<":
+                    tagOpen = True
+                    tagOpenIndex = index
+            else:
+                if letter == "/":
+                    # we assume no nested excluded tags of same type, see notes
+                    continue
+                elif index - tagOpenIndex == 1 and letter in [" "]:
+                    # we are not dealing with a tag here
+                    tagOpen = False
+                    tagOpenIndex = 0
+                    continue
+                elif letter != ">":
+
+                    someTag.handle_letter_unknown_tag(letter)
+                    continue
+                else:
+                    someTag.openingTagClosed = True
+                    tagOpen = False
+                    tagOpenIndex = 0
+
+                    if someTag.tagType in self.excluded_tags:
+                        if someTag.tagType == someTag.consideredTagType:
+                            # we are closing the tag, so let's not ignore content anymore.
+                            ignoreTagContent = False
+                            someTag = Tag(None)
+                        else:
+                            ignoreTagContent = True
+
 
         # split loops so if's evaluated less times
 
@@ -89,3 +191,16 @@ class Extractor:
 
 extractor = Extractor("https://onet.pl")
 extractor.run()
+
+tests("<>")
+tests("<<>")
+tests("<>>")
+
+tests("<\>")
+tests("< >")
+tests("< \>")
+tests("<\    >")
+tests("<a    >")
+tests("< a    >")
+tests("< a class=\"dsadsaads\"   >")
+tests("<a class=\"dsadsaads\"   >")
